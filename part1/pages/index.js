7568c54b70d3b05c488d225a69e5c6e427735ded
Jennet Hydyrova <jennethydyrovaa@gmail.com>
2022-03-23 11:55:53 +0300

    rename vars

--- part1/pages/index.js
+++ part1/pages/index.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import React from "react";
 
-const API = "https://localhost:3443/api";
+const API_URL = "https://localhost:3443/api_URL";
 
 export default function Home() {
   const [data, setData] = React.useState(null);
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(API);
+      const res = await axios.get(API_URL);
       setData(res.data);
       return res.data;
     } catch (e) {
